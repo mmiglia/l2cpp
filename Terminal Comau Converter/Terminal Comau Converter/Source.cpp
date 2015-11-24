aee2856e7c3cc099f912b4523f4e6bd425e89e39
Alessandro Migliardi <ale.migliardi@gmail.com>
2015-11-24 23:54:38 +0100

    Completed the check on lines (is or is not a CARTESIAN_VALUE).
    Fixed the issue with line numbers.
    
    Fixed the issue with the solution to line numbers' issue.

--- Terminal Comau Converter/Terminal Comau Converter/Source.cpp
+++ Terminal Comau Converter/Terminal Comau Converter/Source.cpp
@@ -28,6 +28,8 @@ int main()
 	int lx = 0;
 	int ly = 0;
 	int lz = 0;
+	int l = -2;
+	int n = 0;
 
 	cout << "Starting translation...\n";
 	outfile << "//This is the file intestation. You can freely edit it. Consider describing the shape the file contains.\n";
@@ -35,26 +37,36 @@ int main()
 	while (stop)
 	{
 		getline(infile, inln, delim);
-
 		if (inln == end)
 		{
 			cout << "\nFinished Translation. ";
 			stop = 0;
 		}
-		else if (inln[8+]=='S')
-		{
-			cout << "||   " << inln << ";\n|| Translating to ||\n";
-
-
-			outln = inln + ";";
-			outfile << outln << endl;
-
-		}
 		else
 		{
-			cout << "\n|| Line Ignored   ||\n";
+			while (inln[n] != '=')
+			{
+				n = n + 1;
+				l = l + 1;
+			}
+			if ((inln[3 + l] == 'C')&&(inln[6 + l] == 'T'))
+			{
+				cout << "||   " << inln << ";\n|| Translating to ||\n";
+
+				//Here has to be placed the part that actually transfers the coordinates from the .stp format to the .scl format.
+
+				outln = inln + ";";
+				outfile << outln << endl;
+
+			}
+			else
+			{
+				cout << "\n|| Line Ignored   ||\n";
+			}
 		}
 		getline(infile, inln);
 		inln = "";
+		l = -2;
+		n = 0;
 	}
 }
